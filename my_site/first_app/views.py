06f3bf0521b3a2acf81d08554966a0fd54785e84
JonnySB <jonny-uk@hotmail.com>
2023-06-02 19:24:57 +0100

    committing first_app

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse('THIS IS A VIEW FROM WITHIN MY APP!')
+def simple_view(response):
+    return HttpResponse('SIMPLE VIEW')
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('my_app/', include('my_app.urls')),
+    path('', views.home, name='home'),
+    path('first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
 ]
