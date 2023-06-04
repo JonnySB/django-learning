a9cbbf571699bba3c644dc01b3144df45c27b054
JonnySB <jonny-uk@hotmail.com>
2023-06-04 18:48:49 +0100

    Used reverse to add a page redirect

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
-def simple_view(response):
-    return HttpResponse('SIMPLE VIEW')
+
+articles = {
+    'sports': 'Sports Page',
+    'finance': 'Finance Page',
+    'politics': 'Politics Page',
+}
+
+
+def news_view(request, topic):
+    try:
+        result = articles[topic]
+        return HttpResponse(articles[topic])
+    except:
+        raise Http404('404 GENERIC ERROR')
+
+
+
+def num_page_view(request, num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+
+    webpage = reverse('topic-page', args=[topic])
+    return HttpResponseRedirect(webpage)
