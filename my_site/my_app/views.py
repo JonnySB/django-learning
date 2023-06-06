02c7b4ac67610b3b48d8102545a6ce0ce2848f2f
JonnySB <jonny-uk@hotmail.com>
2023-06-06 11:09:36 +0100

    Added first simple dynamic view to my_app

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def example_view(request):
+    # my_app/templates/MY_APP/EXAMPLE.HTML
+    return render(request, 'my_app/example.html')
+
+def variable_view(request):
+
+    my_var = {
+        'first_name':'Jonny',
+        'last_name':'Brownrigg',
+        'some_list':['list item 1','list item 2','list item 3'],
+        'some_dict':{'inside_key':'inside_value'},
+    }
+
+    return render(
+        request,
+        'my_app/variable.html',
+        context=my_var,
+        )
